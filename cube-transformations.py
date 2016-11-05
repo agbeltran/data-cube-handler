1537389febda35c4d5e01946622d0096d09c507c
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2016-11-05 17:57:10 +0000

    Unpivoted to pivoted tables

--- cube-transformations.py
+++ cube-transformations.py
@@ -2,20 +2,37 @@ import pandas as pd
 
 TAB = '\t'
 
-def table2m_sq(table_filename, output_filename):
+def table2m_sq(table_filename):
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, values='data points', index=['chemical entities'], columns=['samples', 'quantitation types'])
-    pivot_table.to_csv(output_filename, sep=TAB)
+    return pivot_table
 
-def table2ms_q(table_filename, output_filename):
+def table2ms_q(table_filename):
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, values='data points', index=['chemical entities', 'samples'], columns=['quantitation types'])
-    print(pivot_table)
-    pivot_table.to_csv(output_filename, sep=TAB)
+    return pivot_table
+
+def table2sm_q(table_filename):
+    df = pd.read_csv(table_filename, sep=TAB)
+    pivot_table = pd.pivot_table(df, values='data points', index=['samples', 'chemical entities'],
+                                 columns=['quantitation types'])
+    return pivot_table
+
+def table2sq_m(table_filename):
+    df = pd.read_csv(table_filename, sep=TAB)
+    pivot_table = pd.pivot_table(df, values='data points', index=['samples', 'quantitation types'],
+                                 columns=['chemical entities'])
+    return pivot_table
 
 
 def m_sq2sm_q(m_sq_filename):
-    df = pd.read_csv(m_sq_filename, sep=TAB)
+    df_pivot = pd.read_csv(m_sq_filename, sep=TAB)
+    print(df_pivot)
+
+    df = pd.melt(df_pivot, id_vars=['chemical entities'])
+    print("------------")
+    print(df)
+
 
 
 def m_sq2sq_m(m_sq_filename):
@@ -24,8 +41,18 @@ def m_sq2sq_m(m_sq_filename):
     (df.T).to_csv('output.tsv', sep=TAB, index=False)
 
 if __name__ == '__main__':
-    #m_sq2sm_q("m_sq_cube.tsv")
-    #m_sq2sq_m("m_sq_cube.tsv")
-    table2m_sq("table.tsv", "m_sq_output.tsv")
-    table2ms_q("table.tsv", "ms_q_output.tsv")
 
+    pivot_table = table2m_sq("table.tsv")
+    pivot_table.to_csv("m_sq_output.tsv", sep=TAB)
+
+    pivot_table = table2ms_q("table.tsv")
+    pivot_table.to_csv("ms_q_output.tsv", sep=TAB)
+
+    pivot_table = table2sm_q("table.tsv")
+    pivot_table.to_csv("sm_q_output.tsv", sep=TAB)
+
+    pivot_table = table2sq_m("table.tsv")
+    pivot_table.to_csv("sq_m_output.tsv", sep=TAB)
+
+    #m_sq2sm_q("m_sq_cube.tsv")
+    # m_sq2sq_m("m_sq_cube.tsv")
