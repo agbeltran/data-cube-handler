fb0c17dc534303c6a27e79f365a097f6f297be92
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-03-15 17:25:19 +0000

    Handler and test files.

--- datacube/cube_handler.py
+++ datacube/cube_handler.py
@@ -7,6 +7,7 @@ TAB = '\t'
 # S stands for samples
 # Q stands for quantitation types
 
+# The two most important configurations are M_SQ and S_MQ
 
 def table2m_sq(table_filename):
     """
@@ -17,6 +18,24 @@ def table2m_sq(table_filename):
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, index=['chemical entities'], columns=['samples', 'quantitation types'], values='data points')
     return pivot_table
+
+
+def m_sq2s_mq(m_sq_h5_filename):
+    df = pd.read_hdf(m_sq_h5_filename, 'df')
+    t_df = df.stack().stack().reset_index()
+    t_df.columns = ['chemical entities', 'quantitation types', 'samples', 'data points']
+    pivot_table = pd.pivot_table(t_df, values='data points', index=['samples'],
+                                 columns=['chemical entities', 'quantitation types'])
+    return pivot_table
+
+def s_mq2m_sq(s_mq_h5_filename):
+    df = pd.read_hdf(s_mq_h5_filename, 'df')
+    t_df = df.stack().stack().reset_index()
+    t_df.columns = ['chemical entities', 'quantitation types', 'samples', 'data points']
+    pivot_table = pd.pivot_table(t_df, values='data points', index=['chemical entities'],
+                                 columns=['samples', 'quantitation types'])
+    return pivot_table
+
 
 def table2ms_q(table_filename):
     """
@@ -100,125 +119,3 @@ def table2sq_m(table_filename):
 
 
 
-def m_sq2table(m_sq_filename):
-    #df = pd.read_csv(m_sq_filename, sep=TAB, header=None)
-    #table = df.set_index(['samples']) #, 'quantitation types'])#.unstack('quantitation types')
-    #index = pd.MultiIndex(levels=)
-    #df = pd.read_table(m_sq_filename, header=None, index_col=[0],  names=['samples', 'quantitation types'])
-    #df = pd.read_table(m_sq_filename)
-    df = pd.read_csv(m_sq_filename, sep=TAB)
-    print(df)
-    print("-----")
-    print(df.index)
-    table = df.unstack('samples')
-    print(table)
-    return table
-
-
-def ms_q2table(ms_q_filename):
-    """
-    Convert an MS_Q pivoted table to an unpivoted table.
-    :param ms_q_filename:
-    :return:
-    """
-    df = pd.read_csv(ms_q_filename, sep=TAB)
-
-    #df.set_index(['chemical entities' ,'samples']).unstack('quantitation types').fillna(0)
-    #table = df.unstack('quantitation types')
-    table = df.set_index(['chemical entities' ,'samples']).unstack('quantitation types')
-    return table
-
-
-
-def m_qs2table(m_qs_filename):
-    df = pd.read_csv(m_qs_filename, sep=TAB, header=None)
-    print(df)
-    table = df.set_index(['chemical entities']).unstack('quantitation types', 'samples')
-    return table
-
-
-def m_qs2ms_q(m_qs_filename):
-    df = pd.read_csv(m_qs_filename, sep=TAB, header=None)
-
-    print("------------")
-    #row_idx = pd.MultiIndex.from_tuples(df['chemical entities'])
-    print(df)
-    print("------------")
-
-    df = df.unstack()
-    print(df)
-
-
-
-def m_sq2sm_q(m_sq_filename):
-    df = pd.read_csv(m_sq_filename, sep=TAB, header=None)
-    print(df)
-    df.unstack(level=[1,2])
-    print("------------")
-    print(df)
-
-
-
-def m_sq2sq_m(m_sq_filename):
-    df = pd.read_csv(m_sq_filename, sep=TAB)
-    print(df)
-    (df.T).to_csv('output.tsv', sep=TAB, index=False)
-
-def read_pivoted():
-    m_sq_filename = "m_sq_cube.tsv"
-
-    df = pd.read_table(m_sq_filename, header=None)
-
-    array1 = df.ix[:0, 1:].values.tolist()[0]
-    array2 = df.ix[1:1, 1:].values.tolist()[0]
-
-    arrays = [array1, array2]
-
-    tuples = list(zip(*arrays))
-
-    name1 = df.ix[0:0, 0:0].values.tolist()[0][0]
-    name2 = df.ix[1:1, 0:0].values.tolist()[0][0]
-
-    columns = pd.MultiIndex.from_tuples(tuples, names=[name1, name2])
-
-    name3 = df.ix[2:2, 0:0].values.tolist()[0][0]
-
-    array3 = df.ix[3:, 0:0].values.tolist()
-    # flatten list
-    array3 = [item for sublist in array3 for item in sublist]
-
-    index = pd.Index(array3, name=name3)
-
-    values_subset = df.ix[3:, 1:]
-    # values = [tuple( map(lambda x: round(x, 2), t) ) for t in values_subset.values.astype(np.float128)]
-    values = [tuple(t) for t in values_subset.values.astype(np.float128)]
-
-    df_pivoted = pd.DataFrame(values, index=index, columns=columns)
-    return df_pivoted
-
-
-# if __name__ == '__main__':
-#
-#     m_sq2table("m_sq_cube.tsv")
-#     df_pivoted = read_pivoted()
-#     print(df_pivoted)
-
-    #
-    #pivot_table = table2ms_q("table.tsv")
-    #pivot_table.to_csv("ms_q_output.tsv", sep=TAB)
-    #
-    # pivot_table = table2sm_q("table.tsv")
-    # pivot_table.to_csv("sm_q_output.tsv", sep=TAB)
-    #
-    # pivot_table = table2sq_m("table.tsv")
-    # pivot_table.to_csv("sq_m_output.tsv", sep=TAB)
-
-    #m_sq2sm_q("m_sq_cube.tsv")
-    # m_sq2sq_m("m_sq_cube.tsv")
-
-    #m_qs2ms_q("m_qs_cube.tsv")
-
-    #ms_q2table("ms_q_cube.tsv")
-
-    #table = m_qs2table("m_sq_cube.tsv")
-    #table.to_csv('m_qs2table_output.tsv', sep=TAB)
--- tests/cube_handler_test.py
+++ tests/cube_handler_test.py
@@ -11,8 +11,19 @@ class TestCubeHandler(unittest.TestCase):
         self._data_dir = os.path.join(os.path.dirname(__file__), 'data')
         self.TAB = '\t'
         self.table = os.path.join(self._data_dir, "table.tsv")
-        self.m_sq = os.path.join(self._data_dir, "m_sq_cube.tsv")
-        self.m_sq_output = os.path.join(self._data_dir, "m_sq_output.tsv")
+
+        self.m_sq = os.path.join(self._data_dir, "m_sq.tsv")
+        self.m_sq_h5 = os.path.join(self._data_dir, "m_sq.h5")
+        self.m_sq_output = os.path.join(self._data_dir, "m_sq.tsv")
+
+        self.s_mq = os.path.join(self._data_dir, "s_mq.tsv")
+        self.s_mq_h5 = os.path.join(self._data_dir, "s_mq.h5")
+        self.s_mq_output = os.path.join(self._data_dir, "s_mq_output.tsv")
+
+        self.ms_q = os.path.join(self._data_dir, "ms_q.tsv")
+        self.ms_q_h5 = os.path.join(self._data_dir, "ms_q.h5")
+        self.ms_q_output = os.path.join(self._data_dir, "ms_q_output.tsv")
+
         self.table_from_m_sq = os.path.join(self._data_dir, "table_from_m_sq.tsv")
 
     def assertTrue(self, expr, msg=None):
@@ -21,29 +32,32 @@ class TestCubeHandler(unittest.TestCase):
             msg = self._formatMessage(msg, "%s is not true" % safe_repr(expr))
             raise self.failureException(msg)
 
+    ### Test for converting table into M_SQ configuration
     def test_table2m_sq(self):
-        pivot_table = cube_handler.table2m_sq(self.table)
-        pivot_table.to_csv(self.m_sq_output, sep=self.TAB)
+        table = cube_handler.table2m_sq(self.table)
+        table.to_csv(self.m_sq_output, sep=self.TAB)
+        table.to_hdf(self.m_sq_h5, 'df', mode='w', format='fixed')
         self.assertTrue(files_comparison.compare(self.m_sq, self.m_sq_output, True), "Files are not the same!")
-        #------
-        #print(pivot_table.index)
-        #### output:
-        #### Index(['chem1', 'chem2', 'chem3', 'chem4', 'chem5'], dtype='object', name='chemical entities')
-        # -----
-        #print(pivot_table.columns)
-        #### output:
-        # MultiIndex(levels=[['sample1', 'sample2'], ['qt1', 'qt2', 'qt3']],
-        #   labels=[[0, 0, 0, 1, 1, 1], [0, 1, 2, 0, 1, 2]],
-        #   names=['samples', 'quantitation types'])
-        # -----
-        # print("--------  pivot_table before resetting index")
-        # print(pivot_table)
-        # pivot_table = pivot_table.reset_index()
-        # print("--------  pivot_table after resetting index")
-        # print(pivot_table)
-        # -----
 
-    def test_m_sq2table(self):
-        table = cube_handler.m_sq2table(self.m_sq)
-        table.to_csv(self.table_from_m_sq, sep=self.TAB)
-        self.assertTrue(files_comparison.compare(self.table, self.table_from_m_sq, True), "Files are not the same!")
+    ### Test for converting table into S_MQ configuration
+    def test_table2s_mq(self):
+        table = cube_handler.table2s_mq(self.table)
+        table.to_csv(self.s_mq_output, sep=self.TAB)
+        table.to_hdf(self.s_mq_h5, 'df', mode='w', format='fixed')
+        self.assertTrue(files_comparison.compare(self.s_mq, self.s_mq_output, True), "Files are not the same!")
+
+    def test_m_sq2s_mq(self):
+        s_mq_df = cube_handler.m_sq2s_mq(self.m_sq_h5)
+        s_mq_df.to_csv(self.s_mq_output, sep=self.TAB)
+        self.assertTrue(files_comparison.compare(self.s_mq, self.s_mq_output, True), "Files are not the same!")
+
+    def test_s_mq2m_sq(self):
+        m_sq_df = cube_handler.s_mq2m_sq(self.s_mq_h5)
+        m_sq_df.to_csv(self.m_sq_output, sep=self.TAB)
+        self.assertTrue(files_comparison.compare(self.m_sq, self.m_sq_output, True), "Files are not the same!")
+
+    def test_table2ms_q(self):
+        ms_q = cube_handler.table2ms_q(self.table)
+        ms_q.to_csv(self.ms_q_output, sep=self.TAB)
+        ms_q.to_hdf(self.ms_q_h5, 'df', mode='w', format='fixed')
+        self.assertTrue(files_comparison.compare(self.ms_q, self.ms_q_output, True), "Files are not the same!")
