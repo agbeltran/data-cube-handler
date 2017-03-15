518e8738d0ff4e2a2fe5db336e2517cfb88532e8
Alejandra Gonzalez-Beltran <alejandra.gonzalez.beltran@gmail.com>
2017-03-15 14:04:49 +0000

    Changes before including hdf5 code tested elsewhere.

--- datacube/cube_handler.py
+++ datacube/cube_handler.py
@@ -3,58 +3,116 @@ import numpy as np
 
 TAB = '\t'
 
+# M stands for metabolites/chemical entities
+# S stands for samples
+# Q stands for quantitation types
+
 
 def table2m_sq(table_filename):
     """
     Convert a table to a pivoted table with index 'M' and columns 'S' and 'Q'
     :param table_filename:
-    :return:
+    :return: data frame
     """
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, index=['chemical entities'], columns=['samples', 'quantitation types'], values='data points')
     return pivot_table
 
-def m_sq2table(m_sq_filename):
-    #df = pd.read_csv(m_sq_filename, sep=TAB, header=None)
-    #table = df.set_index(['samples']) #, 'quantitation types'])#.unstack('quantitation types')
-    df = pd.read_table(m_sq_filename, index_col=[0,1])
-    print(df)
-    print("-----")
-    print(df.index)
-
-
 def table2ms_q(table_filename):
     """
-
+    Convert a table to a pivoted table with index 'M' and 'S' and column 'Q'
     :param table_filename:
-    :return:
+    :return: data frame
     """
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, values='data points', index=['chemical entities', 'samples'], columns=['quantitation types'])
     return pivot_table
 
+def table2m_qs(table_filename):
+    """
+    Convert a table to a pivoted table with index 'M' and columns 'Q' and 'S'
+    :param table_filename:
+    :return: data frame
+    """
+    df = pd.read_csv(table_filename, sep=TAB)
+    pivot_table = pd.pivot_table(df, index=['chemical entities'], columns=['quantitation types', 'samples'], values='data points')
+    return pivot_table
+
+
+def table2mq_s(table_filename):
+    """
+    Convert a table to a pivoted table with index 'M' and 'Q' and column 'S'
+    :param table_filename:
+    :return: data frame
+    """
+    df = pd.read_csv(table_filename, sep=TAB)
+    pivot_table = pd.pivot_table(df, index=['chemical entities', 'quantitation types'], columns=['samples'], values='data points')
+    return pivot_table
+
+
 def table2sm_q(table_filename):
     """
-
+    Convert a table to a pivoted table with index 'S' and 'M' and column 'Q'
     :param table_filename:
-    :return:
+    :return: data frame
     """
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, values='data points', index=['samples', 'chemical entities'],
                                  columns=['quantitation types'])
     return pivot_table
 
+
+def table2s_mq(table_filename):
+    """
+    Convert a table to a pivoted table with index 'S' and columns 'M' and 'Q'
+    :param table_filename:
+    :return: data frame
+    """
+    df = pd.read_csv(table_filename, sep=TAB)
+    pivot_table = pd.pivot_table(df, values='data points', index=['samples'],
+                                 columns=['chemical entities', 'quantitation types'])
+    return pivot_table
+
+
+def table2s_qm(table_filename):
+    """
+    Convert a table to a pivoted table with index 'S'  and columns 'Q' and 'M'
+    :param table_filename:
+    :return: data frame
+    """
+    df = pd.read_csv(table_filename, sep=TAB)
+    pivot_table = pd.pivot_table(df, values='data points', index=['samples', ],
+                                 columns=['quantitation types', 'chemical entities'])
+    return pivot_table
+
+
+
 def table2sq_m(table_filename):
     """
-
+    Convert a table to a pivoted table with index 'S' and 'Q' and column 'M'
     :param table_filename:
-    :return:
+    :return: data frame
     """
     df = pd.read_csv(table_filename, sep=TAB)
     pivot_table = pd.pivot_table(df, values='data points', index=['samples', 'quantitation types'],
                                  columns=['chemical entities'])
     return pivot_table
 
+
+
+def m_sq2table(m_sq_filename):
+    #df = pd.read_csv(m_sq_filename, sep=TAB, header=None)
+    #table = df.set_index(['samples']) #, 'quantitation types'])#.unstack('quantitation types')
+    #index = pd.MultiIndex(levels=)
+    #df = pd.read_table(m_sq_filename, header=None, index_col=[0],  names=['samples', 'quantitation types'])
+    #df = pd.read_table(m_sq_filename)
+    df = pd.read_csv(m_sq_filename, sep=TAB)
+    print(df)
+    print("-----")
+    print(df.index)
+    table = df.unstack('samples')
+    print(table)
+    return table
 
 
 def ms_q2table(ms_q_filename):
@@ -64,12 +122,12 @@ def ms_q2table(ms_q_filename):
     :return:
     """
     df = pd.read_csv(ms_q_filename, sep=TAB)
-    print(df)
+
     #df.set_index(['chemical entities' ,'samples']).unstack('quantitation types').fillna(0)
     #table = df.unstack('quantitation types')
     table = df.set_index(['chemical entities' ,'samples']).unstack('quantitation types')
-    print("-------")
-    print(table)
+    return table
+
 
 
 def m_qs2table(m_qs_filename):
@@ -139,29 +197,11 @@ def read_pivoted():
     return df_pivoted
 
 
-if __name__ == '__main__':
-
-    pivot_table = table2m_sq("table.tsv")
-
-    pivot_table.to_csv("m_sq_output.tsv", sep=TAB)
-
-    print("--------  pivot_table")
-    print(pivot_table)
-
-    print("----- columns  ")
-    print(pivot_table.columns)
-
-    print("----- index  ")
-    print(pivot_table.index)
-
-    pivot_table = pivot_table.reset_index()
-    print("--------  pivot_table after resetting index")
-
-    print(pivot_table)
-
-    m_sq2table("m_sq_cube.tsv")
-    df_pivoted = read_pivoted()
-    print(df_pivoted)
+# if __name__ == '__main__':
+#
+#     m_sq2table("m_sq_cube.tsv")
+#     df_pivoted = read_pivoted()
+#     print(df_pivoted)
 
     #
     #pivot_table = table2ms_q("table.tsv")
--- tests/cube_handler_test.py
+++ tests/cube_handler_test.py
@@ -10,9 +10,10 @@ class TestCubeHandler(unittest.TestCase):
     def setUp(self):
         self._data_dir = os.path.join(os.path.dirname(__file__), 'data')
         self.TAB = '\t'
-        self.table_file_name = os.path.join(self._data_dir, "table.tsv")
+        self.table = os.path.join(self._data_dir, "table.tsv")
         self.m_sq = os.path.join(self._data_dir, "m_sq_cube.tsv")
         self.m_sq_output = os.path.join(self._data_dir, "m_sq_output.tsv")
+        self.table_from_m_sq = os.path.join(self._data_dir, "table_from_m_sq.tsv")
 
     def assertTrue(self, expr, msg=None):
         """Check that the expression is true."""
@@ -20,8 +21,29 @@ class TestCubeHandler(unittest.TestCase):
             msg = self._formatMessage(msg, "%s is not true" % safe_repr(expr))
             raise self.failureException(msg)
 
-    def test_table2msq(self):
-        pivot_table = cube_handler.table2m_sq(self.table_file_name)
+    def test_table2m_sq(self):
+        pivot_table = cube_handler.table2m_sq(self.table)
         pivot_table.to_csv(self.m_sq_output, sep=self.TAB)
-        files_comparison.compare(self.m_sq, self.m_sq_output, True)
+        self.assertTrue(files_comparison.compare(self.m_sq, self.m_sq_output, True), "Files are not the same!")
+        #------
+        #print(pivot_table.index)
+        #### output:
+        #### Index(['chem1', 'chem2', 'chem3', 'chem4', 'chem5'], dtype='object', name='chemical entities')
+        # -----
+        #print(pivot_table.columns)
+        #### output:
+        # MultiIndex(levels=[['sample1', 'sample2'], ['qt1', 'qt2', 'qt3']],
+        #   labels=[[0, 0, 0, 1, 1, 1], [0, 1, 2, 0, 1, 2]],
+        #   names=['samples', 'quantitation types'])
+        # -----
+        # print("--------  pivot_table before resetting index")
+        # print(pivot_table)
+        # pivot_table = pivot_table.reset_index()
+        # print("--------  pivot_table after resetting index")
+        # print(pivot_table)
+        # -----
 
+    def test_m_sq2table(self):
+        table = cube_handler.m_sq2table(self.m_sq)
+        table.to_csv(self.table_from_m_sq, sep=self.TAB)
+        self.assertTrue(files_comparison.compare(self.table, self.table_from_m_sq, True), "Files are not the same!")
